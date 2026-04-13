3d1ec6235259fa4b4be9546e2f2d9c576800e09d
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-04-13 17:55:16 +0300

    chore(firebase): set up AngularFire, environments, and Firestore config

--- apps/console/src/app/app.config.ts
+++ apps/console/src/app/app.config.ts
@@ -5,11 +5,18 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
